1ebe7e8a4081194a4a7777caca19f380122ab553
dqminh2810 <dqminh2810@gmail.com>
2021-02-17 10:48:56 +0100

    modifying

--- src/test/java/UserTest.java
+++ src/test/java/UserTest.java
@@ -21,9 +21,6 @@ public class UserTest {
     @org.junit.Test
     public void getAge() {
         assertEquals(18, user.getAge());
-        user.setAge(24);
-        assertEquals(24, user.getAge());
-
     }
 
     @org.junit.Test
